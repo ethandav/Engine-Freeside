6ca964c0d622a30e9b287b466bac405320fbc1c4
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2026-05-11 18:42:48 -0700

    efg header

---
+++ src/Engine/efg.Renderer/efg.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "..\..\Engine\efg.Platform\include\Window.h"
+#include "..\..\Engine\efg.Platform\include\Timer.h"
+#include "..\..\Engine\efg.Renderer\include\render\Renderer.h"
+#include "..\..\Engine\efg.Core\include\math\math.h"
+#include "..\..\Engine\efg.Core\include\shapes\shapes.h"
+#include "..\..\Engine\efg.Renderer\include\render\MeshData.h"
+#include "..\..\Engine\efg.Renderer\include\render\Camera.h"
+#include "..\..\Engine\efg.Scene\include\SceneManager.h"
+#include "..\..\Engine\efg.Renderer\include\render\Materials.h"
